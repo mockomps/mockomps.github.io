6a2e8f2c654c8d9278eddf1ca1ec8579fc67e5a3
nina <gpereiranina@gmail.com>
2025-07-16 01:08:45 +0100

    FEAT: Implement PWA improvements and Admin page enhancements

--- js/pages/AdminPage.js
+++ js/pages/AdminPage.js
@@ -13,6 +13,7 @@ export function renderAdminPage(headerContent, mainContent, navigate) {
     mainContent.innerHTML = `
             <div class="grid grid-cols-1 gap-4 text-center mt-0">
                 <button id="add-quali-boulders-btn" class="bg-gray-900 border border-gray-800 p-8 rounded-lg shadow-md hover:shadow-xl hover:bg-gray-800 transition-all duration-200 text-gray-100 font-bold text-xl">Manage Qualis Boulders</button>
+                <button id="submit-quali-images-btn" class="bg-gray-900 border border-gray-800 p-8 rounded-lg shadow-md hover:shadow-xl hover:bg-gray-800 transition-all duration-200 text-gray-100 font-bold text-xl">Submit Quali Boulder Images</button>
                 <button id="manage-finals-btn" class="bg-gray-900 border border-gray-800 p-8 rounded-lg shadow-md hover:shadow-xl hover:bg-gray-800 transition-all duration-200 text-gray-100 font-bold text-xl">Manage Finals Rosters</button>
                 <button id="judge-finals-btn" class="bg-gray-900 border border-gray-800 p-8 rounded-lg shadow-md hover:shadow-xl hover:bg-gray-800 transition-all duration-200 text-gray-100 font-bold text-xl">Judges App</button>
             </div>
@@ -21,4 +22,7 @@ export function renderAdminPage(headerContent, mainContent, navigate) {
     mainContent.querySelector('#add-quali-boulders-btn').addEventListener('click', () => navigate('addBoulders'));
     mainContent.querySelector('#manage-finals-btn').addEventListener('click', () => navigate('manageFinals'));
     mainContent.querySelector('#judge-finals-btn').addEventListener('click', () => navigate('judgesApp'));
+    mainContent.querySelector('#submit-quali-images-btn').addEventListener('click', () => {
+        window.open('https://forms.gle/rzN1ZYh6VzB6CR77A', '_blank');
+    });
 }
